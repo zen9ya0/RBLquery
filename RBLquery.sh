5635974b53db85789dc4b3865cdc4c4155c97ee0
zen9ya0 <solar324yao@gmail.com>
2022-08-08 12:57:56 +0800

    Update RBLquery.sh

--- RBLquery.sh
+++ RBLquery.sh
@@ -2,6 +2,8 @@
 
 PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
+DNS=`grep nameserver /etc/resolv.conf | awk '{print $2}' | tail -n1`
+
 echo "Input You Want to search RBL IP."
 echo "IP: "
 read ip
@@ -10,5 +12,5 @@ RBLIP=`echo -e $ip|awk -F. '{print $4"."$3"."$2"."$1}'`
 
 #echo $RBLIP
 for RBL in bl.spamcop.net cbl.abuseat.org cbl.anti-spam.org.cn dnsbl.sorbs.net pbl.spamhaus.org psbl.surriel.com rbl.softsqr.com;do
-        dig A $RBLIP.$RBL 8.8.8.8|grep 127
+        dig A $RBLIP.$RBL $DNS|grep 127
 done
